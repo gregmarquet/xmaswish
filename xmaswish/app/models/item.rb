502dfc7ec89ba245435006f986bba9b1449c1316
gregmarquet <marquet.greg@gmail.com>
2016-10-09 23:45:56 -0400

    add relationships between user, list, and item

--- xmaswish/app/models/item.rb
+++ xmaswish/app/models/item.rb
@@ -1,2 +1,16 @@
+# == Schema Information
+#
+# Table name: items
+#
+#  id          :integer          not null, primary key
+#  name        :string
+#  description :text
+#  url         :string
+#  list_id     :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Item < ActiveRecord::Base
+  belongs_to :list
 end
--- xmaswish/test/models/item_test.rb
+++ xmaswish/test/models/item_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: items
+#
+#  id          :integer          not null, primary key
+#  name        :string
+#  description :text
+#  url         :string
+#  list_id     :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 require 'test_helper'
 
 class ItemTest < ActiveSupport::TestCase
