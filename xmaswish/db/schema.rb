6f5444f80e0512e3446ef9e8ad5bb64656488d4a
gregmarquet <marquet.greg@gmail.com>
2016-10-10 00:19:52 -0400

    add welcome controller, welcome index and root welcome#index

--- xmaswish/db/schema.rb
+++ xmaswish/db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20161010033713) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "items", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
